9ea9dea04b313e18b5dc0b575d7219c72c68a28d
McheikMohamed <mohamed-mcheik@outlook.fr>
2026-08-26 14:41:07 +0200

    Feat: Création des entités POCO (Administrateur, Membre, Site, Terrain, Match, InscriptionMatch, Paiement, HoraireSite, JourFermeture)

---
+++ Padel.Domain/Entities/Match.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Padel.Domain.Enums;
+
+namespace Padel.Domain.Entities;
+
+public class Match
+{
+    public int MatchId { get; set; }
+    public int TerrainId { get; set; }
+    public string OrganisateurMatricule { get; set; } = string.Empty;
+    public DateTime DateHeureDebut { get; set; }
+    public DateTime DateHeureFin { get; set; }
+    public StatutMatch Statut { get; set; }
+    public decimal Prix { get; set; } = 60m;
+}
---
+++ Padel.Domain/Entities/Terrain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padel.Domain.Entities;
+
+public class Terrain
+{
+    public int TerrainId { get; set; }
+    public int SiteId { get; set; }
+    public int Numero { get; set; }
+}
